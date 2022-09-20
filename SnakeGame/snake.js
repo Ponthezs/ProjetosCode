2f4ae970568966d3635acf19a676962013ab56e1
Felipe Pontes dos Santos <pontesfelipe835@gmail.com>
2022-09-20 15:20:58 -0300

    Code

---
+++ SnakeGame/snake.js
@@ -0,0 +1,53 @@
+ import {getInputDirection} from './input.js'
+ 
+ export const SNAKE_SPEED = 4;
+
+export const snakeBody = [
+    {x: 11, y: 11}
+]
+
+let newSegments = 0
+
+export function update(){
+    addSegments()
+
+    const InputDirection = getInputDirection()
+
+
+    for(let i = snakeBody.lenght - 2; i > 0; i--){
+        snakeBody[i + 1] = {...snakeBody[i]}
+    }
+
+    snakeBody[0].x += InputDirection.x
+    snakeBody[0].y += InputDirection.y
+}
+
+export function draw(gameBoard) {
+    snakeBody.forEach(segment =>{
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridColumnStart = segment.x
+        snakeElement.style.gridRowStart = segment.y
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement)
+    })
+}
+
+export function expandSnake(amount){
+    newSegments += amount
+}
+
+export function addSegments() {
+    for (let i = 0; i < newSegments; i++){
+        snakeBody.push({ ...snakeBody[snakeBody.length -1]})
+    }
+
+    newSegments = 0
+}
+
+export function onSnake(position){
+    return snakeBody.some((segment, index) =>{
+
+        return position.x == segment.x && position.y == segment.y
+    })
+}
+
