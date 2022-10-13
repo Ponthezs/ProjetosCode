b9fdb35da6808f86267a6aed9a5e0cce3fa5255e
Felipe Pontes dos Santos <pontesfelipe835@gmail.com>
2022-10-13 17:57:30 -0300

    JS

--- StrangerThigs/assets/js/data/helfire-clube.js
+++ StrangerThigs/assets/js/data/helfire-clube.js
@@ -1,6 +1,12 @@
 import { addDoc, collection, getFirestore } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'
 import app from './firebase.js'
 
+/**
+ * It takes a subscribe object, gets a firestore instance, gets a collection reference, adds a document
+ * to the collection, and returns the document id.
+ * @param subscribe - {
+ * @returns The id of the document that was created.
+ */
 export async function subscribeToHellfireClube(subscribe) {
     const db = getFirestore(app)
     const hellfireClubCollection = collection(db, 'hellfire-clube')
--- StrangerThigs/assets/js/main.js
+++ StrangerThigs/assets/js/main.js
@@ -1,12 +1,15 @@
 
 import { subscribeToHellfireClube } from './data/hellfire-clube.js'
 
+
+/* Creating a function called main and assigning the values of the inputs to the object. */
 (function main() {
     const txtName = document.getElementById('txtName')
     const txtEmail = document.getElementById('txtEmail')
     const txtLevel = document.getElementById('txtLevel')
     const txtCharacter = document.getElementById('txtCharacter')
 
+/* Creating an object called subscribe and assigning the values of the inputs to the object. */
     document.getElementById('btnSubscribe').addEventListener('click', async () => {
         const subscribe = {
             name: txtName.value,
@@ -15,6 +18,7 @@ import { subscribeToHellfireClube } from './data/hellfire-clube.js'
             character: txtCharacter.value
         }
 
+/* Calling the function subscribeToHellfireClube and passing the subscribe object as a parameter. */
         const id = await subscribeToHellfireClube(subscribe)
         alert(`Inscrição ${id} adicionada com sucesso!`)
     })
