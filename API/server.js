85b9b203518e7dffce0f7def8552e73e8e0670d8
Felipe Pontes <104865606+Ponthezs@users.noreply.github.com>
2024-06-23 17:27:34 -0300

    refactor:  app and express post

--- API/server.js
+++ API/server.js
@@ -1,27 +1,36 @@
 import express from 'express'
 
-const app = express()
 
-const users = []
+const app = express()
+app.use(express.json())
+const user = []
+
 
 app.post('/users', (req, res) => {
 
-    console.log(req)
-    res.send('Seu resgistro foi concluido')
-
+    user.push(req.body)
+    
+    res.status(201).json(req.body)
 
 })
 
 app.get('/users', (req, res) => {
 
-    res.send('Funcionou!')
-
+    res.status(200).json(user)
 
 }) 
 
 app.listen(3000)
 
+
+
+
+
+
+
+
 /* 
+
     Goal: Create our user API
 
     - Creat new user
@@ -29,16 +38,24 @@ app.listen(3000)
     - To edit user
     - Delet one user
 
-
-
     =======================================================
+
     1) Tipo de Rota / Método HTTP
     2) Endereço
 
+    ======================================================
 
     app.post('/users')  Create new user
     app.put('/users')  To edit several user  
     app.patch('/users')  To edit one user  
     app.delete('/users')  Delet user 
     
+    ======================================================
+
+    HTTP Status
+
+    2xx -> success
+    4xx -> Error Client
+    5xx -> Error Server
+
 */ 
