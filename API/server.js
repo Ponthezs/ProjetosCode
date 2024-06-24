af674fe0d94d3749e7d0ac1ae3700b64c7f3fb9f
Felipe Pontes <104865606+Ponthezs@users.noreply.github.com>
2024-06-24 19:11:25 -0300

    Feat

--- API/server.js
+++ API/server.js
@@ -29,7 +29,27 @@ app.get('/users', async (req, res) => {
 
     res.status(200).json(users)
 
-}) 
+})
+
+
+app.put('/users/:id', async (req, res) => {
+
+    await prisma.user.update({
+        whare: {
+            id: req.params.id
+
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+
+    })
+    
+    res.status(201).json(req.body)
+
+})
 
 app.listen(3000)
 
