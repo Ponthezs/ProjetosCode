425a45032261312cedbdcf81ce44dfd4c259dc0c
Felipe Pontes <104865606+Ponthezs@users.noreply.github.com>
2024-08-28 14:48:49 -0300

    feat

--- Bomberman/game.js
+++ Bomberman/game.js
@@ -3,13 +3,14 @@ kaboom({
     fullscreen: true,
     scale: 2,
     debug: true,
-    clearColor: [0,0,0,1]
-})
+    clearColor: [0, 0, 0, 1]
+});
+
 const MOVE_SPEED = 120;
-const ENEMY_SPEED =60;
+const ENEMY_SPEED = 60;
 
-loadRoot('https://i.imgur.com/')
-
+// Carregar sprites
+loadRoot('https://i.imgur.com/');
 loadSprite('wall-steel', 'EkleLlt.png');
 loadSprite('brick-red', 'C46n8aY.png');
 loadSprite('door', 'Ou9w4gH.png');
@@ -17,300 +18,266 @@ loadSprite('kaboom', 'etY46bP.png');
 loadSprite('bg', 'qIXIczt.png');
 loadSprite('wall-gold', 'VtTXsgH.png');
 loadSprite('brick-wood', 'U751RRV.png');
-loadSprite('bomberman', 'T0xbHb8.png',{
+loadSprite('bomberman', 'T0xbHb8.png', {
     sliceX: 7,
     sliceY: 4,
     anims: {
-        //Stoped
-        idleLeft: {from: 21, to: 21},
-        idleRight: {from: 7, to: 7},
-        idleUp: {from: 0, to: 0},
-        idleDown: {from: 14, to: 14},
-
-        //move
-        moveLeft: {from: 22, to: 27},
-        moveRight: {from: 8, to: 13},
-        moveUp: {from: 1, to: 6},
-        moveDown: {from: 15, to: 20}
+        idleLeft: { from: 21, to: 21 },
+        idleRight: { from: 7, to: 7 },
+        idleUp: { from: 0, to: 0 },
+        idleDown: { from: 14, to: 14 },
+        moveLeft: { from: 22, to: 27 },
+        moveRight: { from: 8, to: 13 },
+        moveUp: { from: 1, to: 6 },
+        moveDown: { from: 15, to: 20 }
     }
 });
+loadSprite('baloon', 'z59lNU0.png', { sliceX: 3 });
+loadSprite('ghost', '6YV0Zas.png', { sliceX: 3 });
+loadSprite('slime', 'c1Vj0j1.png', { sliceX: 3 });
+loadSprite('boomber', 'etY46bP.png', { sliceX: 3, anims: { move: { from: 0, to: 2 } } });
+loadSprite('explosion', 'baBxoqs.png', { sliceX: 5, sliceY: 5 });
 
-loadSprite('baloon', 'z59lNU0.png', { sliceX: 3}),
-loadSprite('ghost', '6YV0Zas.png', { sliceX: 3}),
-loadSprite('slime', 'c1Vj0j1.png', { sliceX: 3})
+// Configuração dos mapas
+const maps = [
+    [
+        'aaaaaaaaaaaaaaa',
+        'azzz  &  tzzzza',
+        'azzzazzzzaazzaa',
+        'aazazazazazazaa',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'aaaaaaaaaaaaaaa'
+    ],
+    [
+        'aaaaaaaaaaaaaaa',
+        'a t  zzzzz  t a',
+        'a z aaaa a z a a',
+        'a z a  z a z a a',
+        'a z a  d a z a a',
+        'a z a a  a z a a',
+        'a z a a a z a a a',
+        'a z a  t a z a a a',
+        'a z a a a z a a a',
+        'a z a z a z a a a',
+        'a z a z a z a a a',
+        'a z a z a z a a a',
+        'a z a z a z a a a',
+        'aaaaaaaaaaaaaaa'
+    ],
+    [
+        'aaaaaaaaaaaaaaa',
+        'a  z      z  a',
+        'a z aaaaa a z a',
+        'a z a     a z a',
+        'a z a a a a z a',
+        'a z a a a a z a',
+        'a z a a a a z a',
+        'a z a a a a z a',
+        'a z a a a a z a',
+        'a z a a a a z a',
+        'a z a a a a z a',
+        'a z a a a a z a',
+        'a z a a a a z a',
+        'aaaaaaaaaaaaaaa'
+    ],
+    [
+        'aaaaaaaaaaaaaaa',
+        'azzz  &  }zzzza',
+        'azzzazzzzaazzaa',
+        'aazazazazazazaa',
+        'a  *         a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'a             a',
+        'aaaaaaaaaaaaaaa'
+    ]
+];
 
-loadSprite('boomber', 'etY46bP.png', { 
-    sliceX: 3,
-    anims: {
-        move: { from: 0, to: 2},
-    }
-})
+const levelCfg = {
+    width: 26,
+    height: 26,
+    a: [sprite('wall-steel'), 'wall-steel', solid(), 'wall'],
+    z: [sprite('brick-red'), 'wall-brick', solid(), 'wall'],
+    d: [sprite('brick-red'), 'wall-brick-dool', solid(), 'wall'],
+    b: [sprite('wall-gold'), 'wall-gold', solid(), 'wall'],
+    w: [sprite('brick-wood'), 'wall-brick', solid(), 'wall'],
+    p: [sprite('brick-wood'), 'wall-brick-dool', solid(), 'wall'],
+    t: [sprite('door'), 'door', 'wall'],
+    '}': [sprite('ghost'), 'ghost', 'dangerous', { dir: -1, time: 0 }],
+    '&': [sprite('slime'), 'slime', 'dangerous', { dir: -1, time: 0 }],
+    '*': [sprite('baloon'), 'baloon', 'dangerous', { dir: -1, time: 0 }]
+};
 
-loadSprite('explosion', 'baBxoqs.png', { 
-    sliceX: 5,
-    sliceY: 5,
-})
+// Função para criar uma explosão
+function spawnKaboom(p, frame) {
+    const obj = add([
+        sprite('explosion', { animeSpeed: 0.1, frame }),
+        pos(p),
+        scale(1.5),
+        'kaboom'
+    ]);
 
-scene('game', ({level, score}) => {
+    obj.pushOutAll();
+    wait(0.5, () => {
+        destroy(obj);
+    });
+}
+
+// Função para criar uma bomba
+function spawnBomber(p) {
+    const obj = add([
+        sprite('boomber', { anims: { move: { from: 0, to: 2 } } }),
+        pos(p),
+        scale(1.5),
+        'bomber'
+    ]);
+
+    obj.pushOutAll();
+    obj.play("move");
+
+    wait(4, () => {
+        destroy(obj);
+        const directions = [
+            { dir: vec2(1, 0), frame: 12 }, // Center
+            { dir: vec2(0, -1), frame: 2 }, // Up
+            { dir: vec2(0, 1), frame: 22 }, // Down
+            { dir: vec2(-1, 0), frame: 10 }, // Left
+            { dir: vec2(1, 0), frame: 14 }  // Right
+        ];
+
+        directions.forEach(({ dir, frame }) => {
+            spawnKaboom(obj.pos.add(dir.scale(20)), frame);
+        });
+    });
+}
+
+// Função principal do jogo
+scene('game', ({ level, score }) => {
     layers(['bg', 'obj', 'ui'], 'obj');
 
-    const maps = [
-        [
-            'aaaaaaaaaaaaaaa',
-            'azzz  &  tzzzza',
-            'azzzazzzzaazzaa',
-            'aazazazazazazaa',
-            'a             a',
-            'a             a',
-            'a             a',
-            'a             a',
-            'a             a',
-            'a             a',
-            'a             a',
-            'a             a',
-            'a             a',
-            'a             a',
-            'aaaaaaaaaaaaaaa',
-        ],
-    ]
-
-    const levelCfg = {
-        width: 26,
-        height: 26,
-        a: [sprite('wall-steel'), 'wall-steel', solid(), 'wall'], //PAREDE DE AÇO
-        z: [sprite('brick-red'), 'wall-brick', solid(), 'wall'],//TIJOLO VERMELHO
-        d: [sprite('brick-red'), 'wall-brick-dool', solid(), 'wall'],
-        b: [sprite('wall-gold'), 'wall-gold', solid(), 'wall'], //PAREDE DE OURO
-        w: [sprite('brick-wood'), 'wall-brick', solid(), 'wall'],//PAREDE DE MADEIRA
-        p: [sprite('brick-wood'), 'wall-brick-dool', solid(), 'wall'],
-        t: [sprite('door'), 'door', 'wall'],//PORTA
-        '}': [sprite('ghost'), 'ghost', 'dangerous', { dir: -1, time: 0}],
-        '&': [sprite('slime'), 'slime', 'dangerous', { dir: -1, time: 0}],
-        '*': [sprite('baloon'), 'baloon', 'dangerous', { dir:-1, time: 0}],
-    }
+    add([sprite('bg'), layer('bg')]);
 
     const gameLevel = addLevel(maps[level], levelCfg);
 
-    add([sprite('bg'), layer('bg')])
-
-    const scoreLabel = add ([
-        text('Score:' + score),
+    const scoreLabel = add([
+        text('Score: ' + score),
         pos(400, 30),
         layer('ui'),
         {
-            value: score,
+            value: score
         },
         scale(1)
-    ])
+    ]);
 
-    add([text('Level:' + parseInt(level + 1)), pos(400, 60), scale(1)])
+    add([text('Level: ' + (level + 1)), pos(400, 60), scale(1)]);
 
-    const player = add ([
-        sprite('bomberman', {
-            animeSpeed: 0.1,
-            frame: 14,
-        }),
-        pos(20,190),
-        {dir: vec2(1,0)}
-    ])
+    const player = add([
+        sprite('bomberman', { anims: { idleRight: { from: 7, to: 7 }, moveRight: { from: 8, to: 13 } } }),
+        pos(20, 190),
+        { dir: vec2(1, 0) }
+    ]);
 
-
-    //ACTION PLAYER
+    // Controle do jogador
     player.action(() => {
-        player.pushOutAll()
-    })
+        player.pushOutAll();
+    });
 
     keyDown('left', () => {
-        player.move(-MOVE_SPEED, 0),
-        player.dir = vec2(-1, 0)
-    })
+        player.move(-MOVE_SPEED, 0);
+        player.dir = vec2(-1, 0);
+    });
 
     keyDown('right', () => {
-        player.move(MOVE_SPEED, 0),
-        player.dir = vec2(1, 0)
-    })
+        player.move(MOVE_SPEED, 0);
+        player.dir = vec2(1, 0);
+    });
 
     keyDown('up', () => {
-        player.move(0, -MOVE_SPEED),
-        player.dir = vec2(0, -1)
-    })
+        player.move(0, -MOVE_SPEED);
+        player.dir = vec2(0, -1);
+    });
 
     keyDown('down', () => {
-        player.move(0, MOVE_SPEED),
-        player.dir = vec2(0, 1)
-    })
+        player.move(0, MOVE_SPEED);
+        player.dir = vec2(0, 1);
+    });
 
-    keyPress('left', () => {
-        player.play('moveLeft')
-    })
+    keyPress('left', () => player.play('moveLeft'));
+    keyPress('right', () => player.play('moveRight'));
+    keyPress('up', () => player.play('moveUp'));
+    keyPress('down', () => player.play('moveDown'));
 
-    keyPress('right', () => {
-        player.play('moveRight')
-    })
+    keyRelease('left', () => player.play('idleLeft'));
+    keyRelease('right', () => player.play('idleRight'));
+    keyRelease('up', () => player.play('idleUp'));
+    keyRelease('down', () => player.play('idleDown'));
 
-    keyPress('up', () => {
-        player.play('moveUp')
-    })
+    keyPress('space', () => spawnBomber(player.pos.add(player.dir.scale(0))));
 
-    keyPress('down', () => {
-        player.play('moveDown')
-    })
-
-    keyRelease('left', () => {
-        player.play('idleLeft')
-    })
-
-    keyRelease('right', () => {
-        player.play('idleRight')
-    })
-
-    keyRelease('up', () => {
-        player.play('idleUp')
-    })
-
-    keyRelease('down', () => {
-        player.play('idleDown')
-    })
-
-    keyPress('space', () =>{
-        spawnBomber(player.pos.add(player.dir.scale(0)))
-    })
-
-    //ACTIONS ENEMY
-    action('baloon', (s) => {
+    // Ações dos inimigos
+    function moveEnemy(s) {
         s.pushOutAll();
-        s.move(s.dir * ENEMY_SPEED, 0)
-        s.time = dt();
-        if (s.time <= 0) {
-            s.dir = -s.dir
-            s.time - rand(5)
-        }
-    })
+        s.move(s.dir.scale(ENEMY_SPEED));
+        s.time -= dt();
 
-    action('slime', (s) => {
-        s.pushOutAll();
-        s.move(s.dir * ENEMY_SPEED, 0)
-        s.time = dt();
         if (s.time <= 0) {
-            s.dir = -s.dir
-            s.time - rand(5)
+            s.dir = -s.dir;
+            s.time = rand(5);
         }
-    })
+    }
+
+    action('baloon', moveEnemy);
+    action('slime', moveEnemy);
 
     action('ghost', (s) => {
         s.pushOutAll();
-        s.move(0 , s.dir * ENEMY_SPEED)
-        s.time = dt();
+        s.move(0, s.dir * ENEMY_SPEED);
+        s.time -= dt();
+
         if (s.time <= 0) {
-            s.dir = -s.dir
-            s.time - rand(5)
+            s.dir = -s.dir;
+            s.time = rand(5);
         }
-    })
-    
-    //FUNCTIONS
+    });
 
-    function spawnKaboom (p, frame){
-        const obj = add([
-            sprite('explosion', {
-                animeSpeed: 0.1,
-                frame: frame,
-            }),
-            pos(p),
-            scale(1.5),
-            'kaboom'
-        ])
+    // Colisões
+    player.collides('door', (d) => {
+        go("game", { level: (level + 1) % maps.length, score: scoreLabel.value });
+    });
 
-        obj.pushOutAll();
-        wait(0.5, () => {
-            destroy(obj)
-        })
-    }
+    collides('kaboom', 'dangerous', (k, s) => {
+        camShake(4);
+        wait(1, () => destroy(k));
+        destroy(s);
+        scoreLabel.value++;
+        scoreLabel.text = 'Score: ' + scoreLabel.value;
+    });
 
-    function spawnBomber(p){
-        const obj = add([sprite('boomber'), ('move'), pos(p), scale(1.5), 'bomber'])
-        obj.pushOutAll();
-        obj.play("move");
+    collides('kaboom', 'wall-brick', (k, s) => {
+        camShake(4);
+        wait(1, () => destroy(k));
+        destroy(s);
+    });
 
-        wait(4, () => {
-            destroy(obj);
+    collides('baloon', 'wall', (s) => s.dir = -s.dir);
+    collides('ghost', 'wall', (s) => s.dir = -s.dir);
+    collides('slime', 'wall', (s) => s.dir = -s.dir);
+});
 
-            obj.dir = vec2(1,0)
-            spawnKaboom(obj.pos.add(obj.dir.scale(0)), 12) //Center
-
-            obj.dir = vec2(0,-1)
-            spawnKaboom(obj.pos.add(obj.dir.scale(20)), 2) //Up
-
-            obj.dir = vec2(0, 1)
-            spawnKaboom(obj.pos.add(obj.dir.scale(20)), 22) //Down
-
-            obj.dir = vec2(-1,0)
-            spawnKaboom(obj.pos.add(obj.dir.scale(20)), 10) //left
-
-            obj.dir = vec2(1,0)
-            spawnKaboom(obj.pos.add(obj.dir.scale(20)), 14) //Right
-        })
-    }
-
-    //COLLISIONS
-
-        player.collides('door', (d) => {
-            go("game", {
-                level: (level + 1) % maps.length,
-                score: scoreLabel.value
-            })
-        })
-
-        collides('kaboom', 'dangerous', (k,s) => {
-            camShake(4);
-            wait(1, () => {
-                destroy(k)
-            })
-            destroy(s);
-            scoreLabel.value++
-            scoreLabel.text = 'Score' + scoreLabel.value
-        })
-
-        
-        collides('kaboom', 'wall-brick', (k,s) => {
-            camShake(4);
-            wait(1, () => {
-                destroy(k)
-            })
-            destroy(s);
-        })
-
-        collides('baloon', 'wall', (s) => {
-            s.dir = -s.dir
-        })
-
-        collides('slime', 'wall', (s) => {
-            s.dir = -s.dir
-        })
-
-        collides('ghost', 'wall', (s) => {
-            s.dir = -s.dir
-        })
-
-        collides('kaboom', 'wall-brick-dool', (k,s) => {
-            camShake(4);
-            wait(1, () => {
-                destroy(k);
-            })
-            destroy(s);
-            gameLevel.spawn('t', s.gridPos.sub(0,0))
-        })
-
-        player.collides('dangerous', () => {
-            go('lose', { score: scoreLabel.value })
-        })
-})
-
-scene('lose', ({ score }) => {
-    add([text('Score:' + score, 32), origin('center'), pos(width() / 2, height() / 2 )])
-
-    keyPress('space', () => {
-        go('game', {level: 0, score: 0});
-    })
-})
-    
-go('game', {level: 0, score: 0});
+// Inicialize o jogo
+start('game', { level: 0, score: 0 });
