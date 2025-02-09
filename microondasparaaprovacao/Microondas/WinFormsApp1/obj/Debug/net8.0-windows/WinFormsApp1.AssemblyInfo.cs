eb67d4ecff3ef290d8127b1a85ef57f3bcd36932
Felipe Pontes <104865606+Ponthezs@users.noreply.github.com>
2025-02-09 13:50:07 -0300

    feat

--- microondasparaaprovacao/Microondas/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ microondasparaaprovacao/Microondas/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d0499e2bee800739103e55ad70fc2d002b2e7c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+778b116bc7f204fb9b7f91f5f53fa729f73b1c98")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
